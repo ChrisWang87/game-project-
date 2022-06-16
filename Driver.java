89c487e0667fa17c9a55d44585c8ac44b991e552
ChrisWang87 <goodchris0831@gmail.com>
2022-06-15 20:30:24 -0400

    update

--- Driver.java
+++ Driver.java
@@ -1,5 +1,7 @@
 import javax.swing.JPanel;
 
+import java.awt.Graphics;
+import java.awt.Color;
 import java.awt.event.KeyListener;
 import java.util.Timer;
 import java.awt.event.ActionEvent;
@@ -12,12 +14,19 @@ public class Driver extends JPanel implements KeyListener, ActionListener{
     private Timer timer;
     private int delay = 8;
 
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
     public Driver(){
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        timer = new Timer(delay, this);
-        timer.start();
+//       timer = new Timer(delay, this);
+//        timer.start();
     }
 
     public void paint(Graphics g){ 
@@ -36,8 +45,8 @@ public class Driver extends JPanel implements KeyListener, ActionListener{
         g.fillRect(playerX, 550, 100, 8);
 
         //ball
-        g.setColor(Color.green);
-        g.fillRect(ballposX, ballposy, 20, 20);
+        g.setColor(Color.yellow);
+        g.fillRect(ballposX, ballposY, 20, 20);
 
 
     }
@@ -66,8 +75,32 @@ public class Driver extends JPanel implements KeyListener, ActionListener{
         // TODO Auto-generated method stub
         if(e.getKeyCode() == KeyEvent.VK_RIGHT)
         {
-            
+            if(playerX >= 600){
+                playerX = 600;
+            }
+            else{
+                moveRight();
+            }
         }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT)
+        {
+            if(playerX < 10){
+                playerX = 10;
+            }
+            else{
+                moveLeft();
+            }
+        }
+
+    }
+    public void moveRight(){
+        play = true;
+        playerX += 20;
+    }
+    public void moveLeft(){
+        play = true;
+        playerX -= 20;
     }
     
 }
+
